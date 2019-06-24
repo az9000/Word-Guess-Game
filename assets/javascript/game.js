8c757faedf0bfb692a7869ed70b17c247017552d
Fawzi <alami42@live.com>
2019-06-24 18:36:02 -0500

    some functions and declarations are now in other files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,10 @@
-var footballDictionary = [
-    'backfield',
-    'blitz',
-    'bootleg',
-    'bowl',
-    'carry',
-    'center',
-    'coach',
-    'deflection',
-    'end',
-    'football',
-    'fullback',
-    'goalpost',
-    'gridiron',    
-    'guard',    
-    'halfback', 
-    'heisman',
-    'huddle',
-    'interception',
-    'kicker',
-    'linebacker',
-    'lineman',
-    'scrimmage',
-    'punt',
-    'punter',
-    'QB',
-    'Quarterback',
-    'receiver',
-    'referee',
-    'rush',
-    'sack',
-    'safety',
-    'scrimmage',
-    'spiral',
-    'superbowl',
-    'tackle',
-    'tailback',
-    'tailgate',
-    'touchdown',
-    'turnover',
-    'umpire',
-    'downfield',
-    'facemask',
-    'helmet',
-    'pads',
-    'cleats',
-    'fumble',
-    'pass',
-    'juke',
-    'midfield',
-    'pigskin',    
-    'catch',
-    'down',
-    'defense',
-    'offense',
-    'uprights',
-    'cornerback'
-];
-
-var game = {
-    dictionary: footballDictionary,
+var game = { 
     score: 0,
     attempts: 0,
     maxAttempts: 10,
     word: '',
     randomNumber: -1,
+    lastRandomNumber: -1,
     lettersUsed: [],
     started: false,
     currentWord: '',
@@ -76,8 +17,13 @@ var game = {
         this.started = true;
         this.attempts = 0;        
         this.updateGuesses(this.maxAttempts);
-        this.randomNumber = Math.floor(Math.random() * this.dictionary.length);
-        this.word = this.dictionary[this.randomNumber];
+        this.randomNumber = Math.floor(Math.random() * footballDictionary.length);
+        if (this.lastRandomNumber === this.randomNumber) {
+            // try one more time
+            this.randomNumber = Math.floor(Math.random() * footballDictionary.length);
+        }
+        this.lastRandomNumber = this.randomNumber;
+        this.word = footballDictionary[this.randomNumber];
         //console.log('looking for ' + this.word);
         document.getElementById('current-word').textContent = '';
         for (var i=0; i < this.word.length; i++) {
@@ -134,7 +80,6 @@ var game = {
     },
 
     winner: function() {
-        this.playSound("/assets/sounds/TOUCHDOWN.mp3");
         this.updateScore();
         document.getElementById('num-of-wins').textContent = this.score + '/' + this.gamesPlayed;
         document.getElementById("status-image").style.display = "block";
@@ -147,30 +92,6 @@ var game = {
     playSound: function(src) {
         // TODO
     },
-
-    /**
-     * This is from Google Fu (https://gist.github.com/efenacigiray/9367920)
-     * @param {*} text  : original string
-     * @param {*} index   : index of letter to replace
-     * @param {*} ch : replacement letter
-     */
-    replaceAt: function (text, index, ch) {
-        // text.substr(0, index) = returns left characters up to index
-        // ch = character to use
-        // text.substring(index + 1) returns rightmost characters in text
-        // Example: text = 'hello'
-        // replace 'l' with 'x' (index = 1)
-        // text.substr(0, 1) = 'he'
-        // text.substring(2) = 'lo'
-        // 'he' + 'x' + 'lo' => 'hexlo'
-        // Next round index = 2:
-        // text.substr(0, 2) = 'hex'
-        // text.substring(3) = 'o'
-        // 'hex' + 'x' + 'o' => 'hexxo'
-        return text.substring(0, index) + ch + text.substring(index + 1);
-    }
-    
-
 }
 
 /**
@@ -187,13 +108,13 @@ window.onkeyup = function (evt) {
     if (!game.started) {
         game.newGame();
     } else {
-        if (isAlphabet(evt.key)) {
+        if (utility.isAlphabet(evt.key)) {
             var letterIndex = game.word.toLowerCase().indexOf(evt.key.toLowerCase());
-            console.log(letterIndex);            
+            game.currentWord = document.getElementById('current-word').textContent;
             if (letterIndex >= 0) {
                 for (var i=0; i < game.word.length; i++) {
                     if (game.word[i].toLowerCase() === evt.key.toLowerCase()) {
-                        document.getElementById('current-word').textContent = game.replaceAt(document.getElementById('current-word').textContent, i, evt.key);                        
+                        document.getElementById('current-word').textContent = utility.replaceAt(document.getElementById('current-word').textContent, i, evt.key);                        
                     }
                 }
 
@@ -209,12 +130,4 @@ window.onkeyup = function (evt) {
     }
 }
 
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-function isAlphabet(key) {
-    if (alphabet.indexOf(key.toLowerCase()) >= 0) {
-        return true;
-    }
-    return false;
-}
 
