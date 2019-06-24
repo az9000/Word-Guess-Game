ccaa56401214aa848102c9f1b5a5ff5e7961dcf1
Fawzi <alami42@live.com>
2019-06-24 18:35:16 -0500

    created a separate utility js file to handle common functions

---
+++ assets/javascript/utility.js
@@ -0,0 +1,37 @@
+var utility = {
+
+    alphabet : 'abcdefghijklmnopqrstuvwxyz',
+
+    isAlphabet : function(key) {
+        if (this.alphabet.indexOf(key.toLowerCase()) >= 0) {
+            return true;
+        }
+        return false;
+    },
+
+    replaceAt: function (text, index, ch) {
+        // text.substr(0, index) = returns left characters up to index
+        // ch = character to use
+        // text.substring(index + 1) returns rightmost characters in text
+        // Example: text = 'hello'
+        // replace 'l' with 'x' (index = 1)
+        // text.substr(0, 1) = 'he'
+        // text.substring(2) = 'lo'
+        // 'he' + 'x' + 'lo' => 'hexlo'
+        // Next round index = 2:
+        // text.substr(0, 2) = 'hex'
+        // text.substring(3) = 'o'
+        // 'hex' + 'x' + 'o' => 'hexxo'
+        return text.substring(0, index) + ch + text.substring(index + 1);
+    },
+
+    updateElementText: function(id, text) {
+        if (document.getElementById(id)) {
+            document.getElementById(id).textContent = text;
+        }
+    },
+
+    getElementText: function(id) {
+        return document.getElementById(id).textContent;
+    }
+}
